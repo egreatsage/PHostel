32e94f712451d7b9f01e0ccf466a22cd2bbba93c
GreatSage <michaelcull9715@gmail.com>
2023-04-25 09:11:02 -0700

    fc

--- src/Admin/Pages/Users.jsx
+++ src/Admin/Pages/Users.jsx
@@ -1,30 +1,6 @@
 import React from 'react'
-import { db } from "../../Common/dbconfig";
 
-const Users = () => {
-    const docRef = db.collection('your_collection_name').doc('your_document_id'); // Replace 'your_collection_name' with the actual name of your collection and 'your_document_id' with the actual document ID
-
-// Fetch the document
-docRef.get()
-.then(doc => {
-  if (doc.exists) {
-    // Store the document data in an array
-    const dataArray = [];
-    dataArray.push(doc.data());
-
-    // Get the length of the array
-    const arrayLength = dataArray.length;
-
-    console.log('Document Data:', dataArray);
-    console.log('Array Length:', arrayLength);
-  } else {
-    console.log('Document does not exist');
-  }
-})
-.catch(error => {
-  console.error('Error getting document:', error);
-});
-
+function Users() {
   return (
     <div>Users</div>
   )
--- src/Client/Pages/Booking.jsx
+++ src/Client/Pages/Booking.jsx
@@ -1,35 +1,106 @@
-import { Input, Select } from '@material-tailwind/react'
-import React from 'react'
-
+import { Input, } from '@material-tailwind/react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useUserAuth } from '../../Common/UserAuthContext';
+import dbdataservice from '../../Common/Operations'
+import Swal from 'sweetalert2';
 const Booking = () => {
+  const {user,signUp} = useUserAuth();
+  const [fullname, setfullname] = useState('');
+  const [contact, setcontact] = useState('');
+  const [institution, setinstitution] = useState('');
+  const [emmail, setemmail] = useState('');
+  const [pgname, setpgname] = useState('');
+  const [pgcontact, setpgcontact] = useState('');
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setloading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let userId = user ? user.uid : null; 
+    if (!userId) {
+      return;
+    } else {
+      const newBooking = {
+        fullname, contact, institution, emmail, pgname, pgcontact, userId
+      };
+      try {
+        await dbdataservice.addBooking(newBooking);
+        setloading(true)
+        Swal.fire({
+          text: 'Details Submitted',
+          icon: 'success',
+          timer: 6000,
+          position: 'top-right',
+          confirmButtonText: 'Close'
+        })
+      } catch (err) {
+        Swal.fire({
+          text: 'Problem submitting details, please try again',
+          icon: 'error',
+          timer: 6000,
+          position: 'top-right',
+          confirmButtonText: 'Close'
+        })
+      }
+      setfullname("");
+      setcontact('');
+      setinstitution();
+      setemmail();
+      setpgname();
+      setpgcontact();
+    }
+  };
   return (
-    <div className='my-6'>
-      <form >
+    <div>
+       {showModal && (
+        <MyModal showModal={showModal} setShowModal={setShowModal} />
+      )}
+      <form onSubmit={handleSubmit} >
         <div className='grid md:py-6 w-full md:grid-cols-3 gap-3 overflow-x-hidden'>
-        <div className='my-3'><Input color='teal' type='text' className='text-black' variant='standard' label='Full Name' required/></div>
+        <div className='my-3'><Input
+         value={fullname}
+         onChange={(e)=>setfullname(e.target.value)}
+        color='teal' type='text' className='text-black' variant='standard' label='Full Name' required/></div>
     <div className='my-3 flex items-center mt-9'>
-    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Gender" type='text' label="Select Gender" required>
+    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Gender" type='text' label="Select Gender">
     <option disabled value="Select Gender">Select Gender</option>
   <option value="Male">Male</option>
   <option value="Female">Female</option>
 </select>
     </div>
-      <div className='my-3'><Input color='teal' type='tel' className='text-black' variant='standard' label='Phone Number' required/></div>
-      <div className='my-3'><Input color='teal' type='text' className='text-black' variant='standard' label='Your Institution  ' required/></div>
-      <div className='my-3'><Input color='teal' type='email' className='text-black' variant='standard' label='Your Email' required/></div>
-      <div className='my-3'><Input color='teal' type='text' className='text-black' variant='standard' label='Guardian/Parent Name ' required/></div>
-      <div className='my-3'><Input color='teal' type='tel' className='text-black' variant='standard' label='Guardian/Parent Contact' required/></div>
+      <div className='my-3'><Input color='teal' type='tel' className='text-black' variant='standard' label='Phone Number'
+       value={contact}
+       onChange={(e)=>setcontact(e.target.value)} required/></div>
+      <div className='my-3'><Input color='teal' type='text' className='text-black' variant='standard' label='Your Institution  '
+       value={institution}
+       onChange={(e)=>setinstitution(e.target.value)}
+       required/></div>
+      <div className='my-3'><Input color='teal' type='email' className='text-black' variant='standard' label='Your Email'
+       value={emmail}
+       onChange={(e)=>setemmail(e.target.value)}
+      required/></div>
+      <div className='my-3'><Input color='teal' type='text' className='text-black' variant='standard' label='Guardian/Parent Name '
+       value={pgname}
+       onChange={(e)=>setpgname(e.target.value)}
+      required/></div>
+      <div className='my-3'><Input color='teal' type='tel' className='text-black' variant='standard' label='Guardian/Parent Contact' required
+       value={pgcontact}
+       onChange={(e)=>setpgcontact(e.target.value)}
+      /></div>
       <div className='my-3 flex items-center mt-9'>
-    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Sharing" type='text' label="Room Sharing" required>
+    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Sharing" type='text' label="Room Sharing" >
     <option disabled value="Sharing">Select sharing type</option>
   <option value="Single room">Single room</option>
   <option value="Two sharing">Two sharing</option>
 </select>
 </div>
-      <div className='my-3'><Input color='teal' type='date' className='text-black' variant='standard' label='Check In' required/></div>
+      <div className='my-3'><Input color='teal' type='date' className='text-black' variant='standard' label='Check In' /></div>
       
         </div>
-        <div  className='flex justify-end '> <button type='submit' className='rounded-full bg-[green] text-white font-semibold px-3 py-2'>Submit Booking</button></div>
+        
+        <div  className='flex justify-end '> <button type='submit' className='rounded-full bg-[green] text-white font-semibold px-3 py-1'>Submit Booking</button></div>
       </form>
     </div>
   )
