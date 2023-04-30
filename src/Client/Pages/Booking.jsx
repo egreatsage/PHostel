3bfeb6fee0de5e0fd2bd7c11d193ca021f5f4f9c
GreatSage <michaelcull9715@gmail.com>
2023-04-30 05:54:44 -0700

    fc

--- src/Client/Pages/Booking.jsx
+++ src/Client/Pages/Booking.jsx
@@ -4,26 +4,33 @@ import { useNavigate } from 'react-router-dom';
 import { useUserAuth } from '../../Common/UserAuthContext';
 import dbdataservice from '../../Common/Operations'
 import Swal from 'sweetalert2';
+import Navbar from '../Components/Navbar'
 const Booking = () => {
-  const {user,signUp} = useUserAuth();
+  const {user} = useUserAuth();
   const [fullname, setfullname] = useState('');
   const [contact, setcontact] = useState('');
   const [institution, setinstitution] = useState('');
   const [emmail, setemmail] = useState('');
+  const [gender, setgender] = useState('');
+  const [roomtype, setroomtype] = useState('');
   const [pgname, setpgname] = useState('');
   const [pgcontact, setpgcontact] = useState('');
   const navigate = useNavigate();
-  const [showModal, setShowModal] = useState(false);
-  const [loading, setloading] = useState(false)
-
+ const handleChange1 = (event)=>{
+    gender(event.target.value)
+ }
+ const handleChange2 = (event)=>{
+  roomtype(event.target.value)
+}
   const handleSubmit = async (e) => {
     e.preventDefault();
     let userId = user ? user.uid : null; 
     if (!userId) {
+      navigate("/login")
       return;
     } else {
       const newBooking = {
-        fullname, contact, institution, emmail, pgname, pgcontact, userId
+        fullname, contact, institution, emmail, pgname, pgcontact,roomtype, userId,gender
       };
       try {
         await dbdataservice.addBooking(newBooking);
@@ -31,7 +38,7 @@ const Booking = () => {
         Swal.fire({
           text: 'Details Submitted',
           icon: 'success',
-          timer: 6000,
+          timer: 10000,
           position: 'top-right',
           confirmButtonText: 'Close'
         })
@@ -49,27 +56,29 @@ const Booking = () => {
       setinstitution();
       setemmail();
       setpgname();
-      setpgcontact();
+      setpgcontact('');
+      setgender('');
+      setroomtype('');
     }
   };
   return (
     <div>
-       {showModal && (
-        <MyModal showModal={showModal} setShowModal={setShowModal} />
-      )}
+      <Navbar/>
+      <div className='md:mx-10 mt-32 border rounded-md shadow-lg h-screen px-4'>
       <form onSubmit={handleSubmit} >
-        <div className='grid md:py-6 w-full md:grid-cols-3 gap-3 overflow-x-hidden'>
+        <div className='grid md:py-6 w-full sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 overflow-x-hidden'>
         <div className='my-3'><Input
          value={fullname}
          onChange={(e)=>setfullname(e.target.value)}
         color='teal' type='text' className='text-black' variant='standard' label='Full Name' required/></div>
+   
     <div className='my-3 flex items-center mt-9'>
-    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Gender" type='text' label="Select Gender">
+    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Gender" value={gender} name='Gender' label="Select Gender" onChange={handleChange1}>
     <option disabled value="Select Gender">Select Gender</option>
   <option value="Male">Male</option>
   <option value="Female">Female</option>
 </select>
-    </div>
+    </div>  
       <div className='my-3'><Input color='teal' type='tel' className='text-black' variant='standard' label='Phone Number'
        value={contact}
        onChange={(e)=>setcontact(e.target.value)} required/></div>
@@ -90,18 +99,18 @@ const Booking = () => {
        onChange={(e)=>setpgcontact(e.target.value)}
       /></div>
       <div className='my-3 flex items-center mt-9'>
-    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="Sharing" type='text' label="Room Sharing" >
+    <select className='w-full border-b text-gray-600 border-b-gray-400 cursor-pointer ' id="roomtype" value={roomtype} name='roomtype'  onChange={handleChange2} label="Room Sharing" >
     <option disabled value="Sharing">Select sharing type</option>
   <option value="Single room">Single room</option>
   <option value="Two sharing">Two sharing</option>
 </select>
 </div>
       <div className='my-3'><Input color='teal' type='date' className='text-black' variant='standard' label='Check In' /></div>
-      
-        </div>
-        
+        </div> 
         <div  className='flex justify-end '> <button type='submit' className='rounded-full bg-[green] text-white font-semibold px-3 py-1'>Submit Booking</button></div>
       </form>
+      </div>
+      
     </div>
   )
 }
